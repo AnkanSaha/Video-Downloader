0ea460ddf45af70e8006e7f71b33b0a2bb037bd2
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2022-12-29 10:30:54 +0530

    Removed Cluster Module

--- downloader.js
+++ downloader.js
@@ -1,20 +1,4 @@
-const os = require('os');
-const cluster = require('cluster');
-const Maxworker = os.cpus().length;
-console.log('Maxworker: ', Maxworker);
-if(cluster.isMaster){
-  let startcluster = 0;
-  while (startcluster<Maxworker){
-    cluster.fork();
-    startcluster++;
-  }
-  cluster.on('exit', ()=>{
-    console.log('A worker is died');
-    cluster.fork();
-  })
-}
-else{
-  const express = require("express"); // Importing Express JS
+ const express = require("express"); // Importing Express JS
 const app = express(); //Initelizing Express JS
 const BodyParser = require("body-parser"); // Importing Body-Parser Module
 const port = 5500; // Default Port No For App Start
@@ -34,4 +18,3 @@ app.use(GetRouting);
 app.listen(port, () => {
   console.log(`Server Started on Port No ${port}`);
 });
-}

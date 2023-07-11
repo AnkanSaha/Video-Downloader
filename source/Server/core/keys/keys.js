392260915380a8a21c88246690b8918edeca8311
Ankan Saha <ankansahadaluabari@gmail.com>
2023-07-11 23:32:56 +0530

    Fixed

--- source/Server/core/keys/keys.js
+++ source/Server/core/keys/keys.js
@@ -1,14 +1,17 @@
 require('dotenv').config(); // Environment Variables
 
-// Allowed URLS
-const AllowedURLS = ['video.daluabari.in', 'daluabari.in', 'localhost:5500'];
-
 const PORT =    Number(process.env.PORT) || 5500; // Default Port No For App Start
 
 // Allowed Origins
 const CORS = {
     AllowedOrigins: String(process.env.CORSALLOWEDURL)
 }; // CORS Allowed Origins
+
+
+// Allowed URLS
+const AllowedURLS = [CORS.AllowedOrigins.split("https://").join(""), CORS.AllowedOrigins.split("https://video.").join(""), 'localhost:5500'];
+
+console.log(AllowedURLS);
 
 const RapidAPI = {
     RapidAPIKey: String(process.env.RAPIDAPIKEY),
